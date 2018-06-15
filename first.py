6b478b109c9a728ea0a62e7fc377fbf6643bf01e
kthayyil <kevin.thayyil@gmail.com>
2018-06-15 17:03:27 -0500

    something was missing last time. added that

--- first.py
+++ first.py
@@ -7,7 +7,24 @@ import csv
 numRows = 10
 numCols = 15
 i = 0
+cordinate = [0,0]
+masterlist = []
+
 for row in range(numRows):
     for col in range(numCols):
-        # print(i)
-        # i += 1
+        cordinate[0] = row
+        cordinate[1] = col
+        # print(cordinate)
+        masterlist.append(cordinate)
+        # print(masterlist)
+print(masterlist)
+plotFile = open('plot.csv')
+# exampleReader = csv.reader(exampleFile)
+# print(list(exampleReader))
+
+csvWriter = csv.writer(plotFile)
+for current in masterlist:
+    # csvWriter.writerow(masterlist)
+    print(current)
+
+THis is stupid shit that needs undoing
